b40ba5e43a649eb04ed4b1abeea4c794e54062cd
Etheriouse <liayel@Zera.>
2025-04-22 01:41:59 +0200

    on met en place le sol

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.RenderingHints.Key;
 import java.awt.event.KeyEvent;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Vector;
 
@@ -9,25 +10,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         Window w = new Window("Evolution System");
 
-        Particule p1 = new Particule(new vect(100, 450), new vect(0, 0), 2, 25);
-        Particule p2 = new Particule(new vect(100, 550), new vect(0, 0), 1, 25);
-
-        Stick s = new Stick(p1, p2, 100);
-
         int left = KeyEvent.VK_Q, right = KeyEvent.VK_D, up = KeyEvent.VK_Z, down = KeyEvent.VK_S, space = KeyEvent.VK_SPACE, escape = KeyEvent.VK_ESCAPE;
-
 
         boolean start = false;
 
-        double dt = 0.16;
+        double dt = 0.1;
+        double tick = 0;
 
+        Entity er = new Entity(50, 50, 10, 700);
 
         while (!w.keypressed(escape)) {
             w.clear(Color.gray);
-        
-            s.draw(w);
-            p1.draw(w);
-            p2.draw(w);
+
+
+            for(int i = 0; i<5000; i+=100) {
+                w.drawLine(i, (int) (Particule.floor(i)), i+100, (int) (Particule.floor(i+100)), 4, Color.black);
+            }
+
+            //e.draw(w);
+            er.draw(w);
 
             if(w.keypressed(space) && w.cooldown(space, 200)) {
                 w.resetcooldown(space);
@@ -35,38 +36,37 @@ public class App {
                 start = !start;
             }
 
-            if(w.keypressed(left) && w.cooldown(left, 200)) {
-                w.resetcooldown(left);
-                p1.addStrenght(new vect(-10, 0), dt);
-            }
-            
-            if(w.keypressed(right) && w.cooldown(right, 200)) {
-                w.resetcooldown(right);
-                p1.addStrenght(new vect(10, 0), dt);
-            }
+            // if(w.keypressed(left) && w.cooldown(left, 200)) {
+            //     w.resetcooldown(left);
+            //     p1.addStrenght(new vect(-10, 0));
+            // }
 
-            if(w.keypressed(up) && w.cooldown(up, 200)) {
-                w.resetcooldown(up);
-                p1.addStrenght(new vect(0, -10), dt);
-            }
+            // if(w.keypressed(right) && w.cooldown(right, 200)) {
+            //     w.resetcooldown(right);
+            //     p1.addStrenght(new vect(10, 0));
+            // }
 
-            if(w.keypressed(down) && w.cooldown(down, 200)) {
-                w.resetcooldown(down);
-                p1.addStrenght(new vect(0, 10), dt);
-            }
+            // if(w.keypressed(up) && w.cooldown(up, 200)) {
+            //     w.resetcooldown(up);
+            //     p1.addStrenght(new vect(0, -10));
+            // }
+
+            // if(w.keypressed(down) && w.cooldown(down, 200)) {
+            //     w.resetcooldown(down);
+            //     p1.addStrenght(new vect(0, 10));
+            // }
 
             if (start) {
-                System.out.println("p1");
-                p1.process(dt);
-                System.out.println("p2");
-                p2.process(dt);
-                System.out.println("fix");
-                s.fix_();
+                er.process(dt, tick);
+
+                //e.process(dt);
             }
+
+            tick += dt;
 
             w.refresh();
         }
-        w.exit();        
+        w.exit();
     }
 }
 
@@ -102,8 +102,18 @@ class vect {
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
+    public vect normalize() {
+        double len = norm();
+        return len > 0 ? this.scale(1.0 / len) : new vect(0, 0);
+    }
+
     public vect other() {
         return new vect(x, y);
+    }
+
+    public void reset() {
+        x = 0;
+        y = 0;
     }
 
     @Override
@@ -114,31 +124,83 @@ class vect {
 
 class Particule {
 
+
+
     public vect position;
+    public vect prev_pos;
     public vect velocity;
     public double mass;
-    public double radius;
 
-    public Particule(vect pos, vect vel, double mass, double radius) {
+    class Movement {
+        public vect strength;
+        public double tick_len;
+
+        public Movement(vect strength, double tick_len) {
+            this.strength = strength;
+            this.tick_len = tick_len;
+        }
+    }
+    public ArrayList<Movement> Movements = new ArrayList<>();
+    public int actual_mov = 0;
+    public double start_dt_actual = 0;
+
+    public Particule(vect pos, vect vel, double mass, double strenght, double endurance, int inteligence) {
         position = pos;
+        prev_pos = pos.other();
         velocity = vel;
         this.mass = mass;
-        this.radius = radius;
+
+        for(int i = 0; i<inteligence; i++) {
+            double tx = Math.random()*strenght;
+            double ty = Math.random()*strenght;
+            Movements.add(new Movement(new vect(tx*(Math.random()*10%2==0?-1:1), ty*(Math.random()*10%2==0?-1:1)), Math.random()*endurance + 1));
+        }
     }
 
-    public void addStrenght(vect strenght, double dt) {
-        vect acc = strenght.scale(1.0/mass);
-        velocity.add(acc.scale(dt));
+    public void mov(double tick) {
+        if(actual_mov < Movements.size()) {
+            if(start_dt_actual+ Movements.get(actual_mov).tick_len > tick) {
+                velocity.add(Movements.get(actual_mov).strength);
+            } else {
+                start_dt_actual = tick;
+                actual_mov++;
+            }
+        } else {
+            actual_mov = 0;
+            start_dt_actual = 0;
+        }
+    }
+
+    public void addStrenght(vect strenght) {
+        velocity.add(strenght);
     }
 
     public void process(double dt) {
-        System.out.println("avant: " + position);
-        position.add(velocity.other().scale(dt));
-        System.out.println("apres: " + position);
+        vect new_prev_pos = position.other();
+        vect acc = velocity.other().scale(1.0/mass);
+        position.add(position.other().sub(prev_pos)).add(acc.scale(dt*dt));
+        prev_pos = new_prev_pos;
+        velocity.reset();
+        if(position.y > floor(position.x)) {
+            position.y = floor(position.x);
+            prev_pos.y = floor(position.x);
+        }
     }
 
     public void draw(Window w) {
-        w.drawCircleFill((int) (position.x-(radius/2)), (int) (position.y-(radius/2)), (int)radius, Color.cyan);
+        w.drawCircleFill((int) (position.x-(((mass*10/2)))), (int) (position.y-((mass*10)/2)), (int)mass*10, Color.cyan);
+    }
+
+    @Override
+    public String toString() {
+        return "Position: " + position.toString() + " Velocity: " + velocity.toString() + " Mass: " + mass;
+    }
+
+    public static double floor(double x) {
+        return (Math.sin(x)
+        + 0.5 * Math.sin(3*x + 1.5)
+        + 0.25 * Math.cos(x *5))*100 + 1000;
+        //sin(x)+0.5 sin(3 x+1.5)+0.25 sin(5 x)
     }
 
 }
@@ -148,10 +210,10 @@ class Stick {
     public Particule p1, p2;
     public double size;
 
-    public Stick(Particule p1, Particule p2, double size) {
+    public Stick(Particule p1, Particule p2) {
         this.p1 = p1;
         this.p2 = p2;
-        this.size = size;
+        this.size = p1.position.other().sub(p2.position).norm();
     }
 
     public void fix_() {
@@ -159,23 +221,126 @@ class Stick {
         if(delta.norm() == size) {
             return;
         }
-        double dist = delta.norm();
-        double diff = (dist - size) / dist;
-
-        double w1 = 1.0/p1.mass, w2 = 1.0/p2.mass, total = w1 + w2;
-        vect correct = delta.other().scale(diff);
-
-        p1.position.add(correct.other().scale(w1/total));
-        p2.position.add(correct.other().scale(w2/total));
+        double d = delta.norm();
+        double error = d - size;
+        vect correct = delta.scale(error / d).scale(0.5);
+        p1.position.add(correct);
+        p2.position.sub(correct);
     }
 
     public void draw(Window w) {
         w.drawLine((int)p1.position.x, (int) p1.position.y, (int) p2.position.x, (int) p2.position.y, 5, Color.white);
     }
+
+    @Override
+    public String toString() {
+        return "Size: " + size;
+    }
 }
 
 class Entity {
 
-    public ArrayList<Stick> sticks = new ArrayList<>();
+    public ArrayList<Stick> Sticks = new ArrayList<>();
+    public ArrayList<Particule> Particules = new ArrayList<>();
+
+    public Entity(int nb_max_particules, int nb_max_sticks, int max_mass, int max_size_stick) {
+        int nb_particules = (int) (Math.random() * nb_max_particules) + 1;
+        int nb_sticks = (int) (Math.random() * nb_max_sticks) + 1;
+
+        double endurance = Math.random()*10;
+        double strength = Math.random()*200;
+        int inteligence =  (int) Math.random()*5 + 1;
+
+        for (int i = 0; i < nb_particules; i++) {
+            Particule p = new Particule(new vect((int) (Math.random() * max_size_stick), (int) (Math.random() * max_size_stick)), new vect(0, 0), Math.random() * max_mass + 1, strength, endurance, inteligence);
+            Particules.add(p);
+        }
+
+        for(int i = 0; i < nb_sticks; i++) {
+            int p1 = (int) (Math.random() * nb_particules);
+            int p2 = (int) (Math.random() * nb_particules);
+            while(p1 == p2) {
+                p2 = (int) (Math.random() * nb_particules);
+            }
+            Stick s = new Stick(Particules.get(p1), Particules.get(p2));
+            Sticks.add(s);
+        }
+
+        Particules.removeIf(p -> Sticks.stream().noneMatch(s -> s.p1 == p || s.p2 == p));
+        ArrayList<Particule> connected = new ArrayList<>();
+        ArrayList<Stick> connectedSticks = new ArrayList<>();
+        connected.add(Particules.get(0));
+
+        for (int i = 0; i < connected.size(); i++) {
+            Particule current = connected.get(i);
+            for (Stick s : Sticks) {
+            if (s.p1 == current && !connected.contains(s.p2)) {
+                connected.add(s.p2);
+                connectedSticks.add(s);
+            } else if (s.p2 == current && !connected.contains(s.p1)) {
+                connected.add(s.p1);
+                connectedSticks.add(s);
+            }
+            }
+        }
+
+        Particules = connected;
+        Sticks = connectedSticks;
+    }
+
+    public Entity(Particule p[], Stick s[]) {
+        for (Particule p1 : p) {
+            Particules.add(p1);
+        }
+        for (Stick s1 : s) {
+            Sticks.add(s1);
+        }
+    }
+
+    public void draw(Window w) {
+        for (Stick s : Sticks) {
+            s.draw(w);
+        }
+        for (Particule p : Particules) {
+            p.draw(w);
+        }
+    }
+
+    public void process(double dt, double tick) {
+        for (Particule p : Particules) {
+            p.mov(tick);
+            p.process(dt);
+        }
+        gravity();
+        for (Stick s : Sticks) {
+            s.fix_();
+        }
+    }
+
+    public void mov(vect pos) {
+        for (Particule p : Particules) {
+            p.position.add(pos);
+        }
+    }
+
+    public void gravity() {
+        for (Particule p : Particules) {
+            p.addStrenght(new vect(0, 9.81 * p.mass));
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Particules: \n");
+        for (Particule p : Particules) {
+            sb.append(p.toString()).append("\n");
+        }
+        sb.append("Sticks: \n");
+        for (Stick s : Sticks) {
+            sb.append(s.toString()).append("\n");
+        }
+        return sb.toString();
+    }
 
 }
